31b444b15be6e4d7c980679219b687917333b8e6
lducr <lauriane.ducrot@gmail.com>
2013-09-23 11:35:53 +0200

    Fusion des fichiers

--- accueil.php
+++ accueil.php
@@ -9,48 +9,5 @@
     </head>
     <body>
         
-        <!-- NAV -->
-        <div class="navbar navbar-default navbar-inverse" role="navigation">
-            <!-- Partie de la barre toujours affichée -->
-            <div class="navbar-header">
-                <!-- Bouton affiché à droite si la zone d'affichage est trop petite -->
-                <button type="button" class="navbar-toggle" data-toggle="collapse" datatarget=".navbar-collapse">
-                    <span class="sr-only">Activer la navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="accueil.php">Chic Tendance</a>
-            </div>
-            <!-- Partie de la barre masquée en fonction de la zone d'affichage -->
-            <div class="collapse navbar-collapse navbar-right">
-                <ul class="nav navbar-nav">
-                    <li><a href="#">Inscription</a></li>
-                    <li><a href="#">Se connecter</a></li>
-                    <li><a href="#">Panier</a></li>
-                </ul>
-            </div>
-        </div>
-                <!-- FIN NAV -->
-       
-        <!-- MENU -->
-        <div class="row">
-            <!-- Taille 4 sur tablettes, 3 sur desktop et plus -->
-            <div class="col-sm-4 col-md-4">
-                <a href="accueil.php">Accueil</a>
-            </div>
-            <!-- Taille 6 sur tablettes, 7 sur desktop et plus -->
-            <div class="col-sm-6 col-md-4">
-            <a href="nouveaute.html">Nouveauté</a>
-            </div>
-            <!-- Taille 2 sur tablettes et plus -->
-            <div class="col-sm-4">
-                Style
-            </div>
-        </div>
-                <!-- FIN MENU -->
-                
-                <img href="" />
-        
     </body>
 </html>
