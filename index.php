5d372b6b8c92bd0b4919ee4d1830d4483ef43c1f
Lauriane <Lauriane@Lauriane-TOSH.home>
2013-09-23 22:42:38 +0200

    modification_3

--- index.php
+++ index.php
@@ -11,8 +11,13 @@
     <?php include("header.html"); ?>
     
     <body>
-        <img class="robe" src="images/robe_bleue.jpg" />
-        <img class="robe" src="images/robe_rouge.jpg" />
-        <img class="robe" src="images/robe_violette.jpg" />
+
+        <div class="imgAccueil"><img class="imgAccueilRobeSoiree" src="images/robe_bleue.jpg" />
+        <img class="imgAccueilRobeMariee" src="images/robe_mariee.jpg" />
+        <img class="imgAccueilRobeCocktail" src="images/robe_cocktail.jpeg" /></div>
+
+        <p>Chic Tendance vous présente ces plus belles créations en robes de soirées, robes de mariée et robes de cocktails ! Grâce à elles vous êtes sûr d'être à la pointe de la mode. Il y en a assez pour satisfaire tout les goûts, alors n'hésitez plus !</p>
+        
+        
     </body>
 </html>
