460683407f71d5642aa515e839f110e249a143d8
lducr <lducr@TRAN-4015-030.classic.local>
2013-09-23 10:37:23 +0200

    Ajout des sources

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <title>Accueil</title>
+    </head>
+    
+    <div class="navbar navbar-default navbar-inverse" role="navigation">
+        <!-- Partie de la barre toujours affichée -->
+        <div class="navbar-header">
+        <!-- Bouton affiché à droite si la zone d'affichage est trop petite -->
+            <button type="button" class="navbar-toggle" data-toggle="collapse" datatarget=".
+        navbar-collapse">
+                <span class="sr-only">Activer la navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Chic Tendance</a>
+        </div>
+        <!-- Partie de la barre masquée en fonction de la zone d'affichage -->
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Inscription</a></li>
+                <li><a href="#">Se Connecter</a></li>
+                <li><a href="#">Panier</a></li>
+            </ul>
+        </div>
+    </div>
+    
+    <nav>
+<!--        <a href="#">ACCUEIL</a>
+        <a href="#">NOUVEAUTES</a>
+        <a href="#">STYLES</a>-->
+    <div class="row-xs-4">
+        <ul class="nav nav-justified nav-stacked">
+            <li class="active"><a href="">ACCUEIL</a></li>
+            <li><a href="">NOUVEAUTES</a></li>
+            <li><a href="">STYLES</a></li>
+        </ul>
+    </div>
+    </nav>
+    
+    <body>
+        
+    </body>
+</html>
