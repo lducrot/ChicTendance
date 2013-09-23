b5df7a4680c4f24ff64c84658200c7754f96d04a
lducr <lauriane.ducrot@gmail.com>
2013-09-23 11:17:11 +0200

    Fusion des fichiers

--- accueil.php
+++ accueil.php
@@ -4,7 +4,7 @@
         <title>Chic Tendance - Accueil</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-        <LINK rel="stylesheet" type="text/css" href="style.css">
+        <LINK rel="stylesheet" type="text/css" href="design.css">
         <link href='images/favicon.ico' type='image/x-icon' rel='shortcut icon' />
     </head>
     <body>
