a934a6d44bc83522cf8ea021c953919d91e7a928
Lauriane <Lauriane@Lauriane-TOSH.home>
2013-09-24 17:02:35 +0200

    modification_3

--- accueil.php
+++ accueil.php
@@ -4,8 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-        <LINK rel="stylesheet" type="text/css" href="design.css">
-        
+        <LINK rel="stylesheet" type="text/css" href="design.css">      
         <link rel="icon" type="image/png" href="images/icone.png" />
         <title>Accueil</title>
     </head>
