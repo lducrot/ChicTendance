b5e13e24a8dccf2ba03f9dc095abf310ee76c0b4
Gwendoline <gomez.gwendo@orange.fr>
2013-09-25 22:54:27 +0200

    Mise à jour robes de mariée

--- robeMariee.php
+++ robeMariee.php
@@ -10,6 +10,100 @@
          <li><a class="filAriane" href="style.php">Style</a></li>
          <li class="active">Robes de mariées</li>
     </ul>
+        
+        <div class="row">
+        <!--Robe 1-->
+        <div class="col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="images/robe_mariee_1.jpg" title="Robe de cocktail rouge" />
+            <div class="caption">
+              <h3>Robe de mariée élégante</h3>
+              <p>Vous avez envie de vous faire remarquer ? Cette robe tres élégante est parfaite pour vous !<br /><br />EUR 89.99</p>
+              <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
+            </div>
+          </div>
+        </div>
+        <!--Robe 2-->
+        <div class="col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="images/robe_mariee_2.jpg" title="Robe de cocktail noire" />
+            <div class="caption">
+              <h3>Robe de mariée dentelle</h3>
+              <p>Parfaite pour un mariage d'exception, cette robe élégante vous ira à ravir !<br /><br />EUR 99.99</p>
+              <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
+            </div>
+          </div>
+        </div>
+        <!--Robe 3-->
+        <div class="col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="images/robe_mariee_3.jpg" title="Robe de cocktail rose" />
+            <div class="caption">
+              <h3>Robe de mariée perlée</h3>
+              <p>En portant cette robe, tous les regards se tourneront vers vous. N'hésitez plus elle est faite pour vous !<br />EUR 85.99</p>
+              <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
+            </div>
+          </div>
+        </div>
+        <!--Robe 4-->
+        <div class="col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="images/robe_mariee_4.jpg" title="Robe de cocktail verte" />
+            <div class="caption">
+              <h3>Robe de mariée plume</h3>
+              <p>Grâce à cette magnifique robe votre mariage sera un réel compte de fées et vous rendrez jalouse toutes les femmes.<br />EUR 79.99</p>
+              <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
+            </div>
+          </div>
+        </div>
+        </div>
+        <div class="row">
+        <!--Robe 5-->
+        <div class="col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="images/robe_mariee_5.jpg.jpg" title="Robe de cocktail bleue" />
+            <div class="caption">
+              <h3>Robe de mariée duvet</h3>
+              <p>Cette robe fera de vous la plus convoitée femmes !<br /><br />EUR 79.99</p>
+              <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
+            </div>
+          </div>
+        </div>
+        <!--Robe 6-->
+        <div class="col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="images/robe_mariee_6.jpg.jpg" title="Robe de cocktail violette" />
+            <div class="caption">
+              <h3>Robe de mariée</h3>
+              <p>Cette robe originale brillera de mille feux sur vous.<br /><br />EUR 79.99</p>
+              <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
+            </div>
+          </div>
+        </div>
+        <!--Robe 7-->
+        <div class="col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="images/robe_mariee_7.jpg.jpg" title="Robe de cocktail blanche" />
+            <div class="caption">
+              <h3>Robe de mariée légère</h3>
+              <p>Grâce à cette robe simple et aérienne vous serez la princesse de cet évènement.<br />EUR 85.99</p>
+              <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
+            </div>
+          </div>
+        </div>
+        <!--Robe 8-->
+        <div class="col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="images/robe_mariee_8.jpg" title="Robe de cocktail corail" />
+            <div class="caption">
+              <h3>Robe de mariée classe</h3>
+              <p>En portant cette robe proche du corps vous deviendrez la plus féminine et désirée des femmes.<br /><br />EUR 89.99</p>
+              <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
+            </div>
+          </div>
+        </div>
+        </div>
+        
     </body>
      <?php include("footer.html"); ?>
 </html>
