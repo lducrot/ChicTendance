b94bd4b9c56e9342583fb7d43eb3e25692b2cb85
lducr <lauriane.ducrot@gmail.com>
2013-10-01 09:44:16 +0200

    motif robe soiree, mariee, cocktail

--- robeCocktail.php
+++ robeCocktail.php
@@ -14,6 +14,7 @@
     <div class="row">
     <div class="col-sm-6 col-md-2">
         <ul class="listMenuStyle">
+            <h1 class="enTete">STYLES</h1>
             <a href="robeSoiree.php"><li>Robe de soirée</li></a>
             <a href="robeCocktail.php"><li>Robe de cocktail</li></a>
             <a href="robeMariee.php"><li>Robe de mariée</li></a>
@@ -22,10 +23,10 @@
     <!--Robe 1-->
     <div class="col-sm-6 col-md-2">
       <div class="thumbnail">
-        <img src="Contenu/images/robe_cocktail_rouge.jpg" title="Robe de cocktail rouge" />
+        <img class="robeCocktail" src="Contenu/images/robe_cocktail_rouge.jpg" title="Robe de cocktail rouge" />
         <div class="caption">
           <h3>Robe de cocktail rouge</h3>
-          <p>Vous avez envie de vous faire remarquer ? Cette robe extremement tendance est parfaite pour vous !<br /><br />EUR 89.99</p>
+          <!--<p>Vous avez envie de vous faire remarquer ? Cette robe extremement tendance est parfaite pour vous !--><br /><br />EUR 89.99
           <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
         </div>
       </div>
@@ -33,10 +34,10 @@
     <!--Robe 2-->
     <div class="col-sm-6 col-md-2">
       <div class="thumbnail">
-        <img src="Contenu/images/robe_cocktail_noire.jpg" title="Robe de cocktail noire" />
+        <img class="robeCocktail" src="Contenu/images/robe_cocktail_noire.jpg" title="Robe de cocktail noire" />
         <div class="caption">
           <h3>Robe de cocktail noire</h3>
-          <p>Parfaite pour une soirée chic, cette robe élégante vous ira à ravir !<br /><br />EUR 99.99</p>
+          <!--<p>Parfaite pour une soirée chic, cette robe élégante vous ira à ravir !--><br />EUR 99.99
           <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
         </div>
       </div>
@@ -44,10 +45,10 @@
     <!--Robe 3-->
     <div class="col-sm-6 col-md-2">
       <div class="thumbnail">
-        <img src="Contenu/images/robe_cocktail_rose.jpg" title="Robe de cocktail rose" />
+        <img class="robeCocktail" src="Contenu/images/robe_cocktail_rose.jpg" title="Robe de cocktail rose" />
         <div class="caption">
           <h3>Robe de cocktail rose</h3>
-          <p>En portant cette robe aucun homme ne pourra vous resister. N'hésitez plus elle est faite pour vous !<br />EUR 85.99</p>
+          <!--<p>En portant cette robe aucun homme ne pourra vous resister. N'hésitez plus elle est faite pour vous !--><br />EUR 85.99
           <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
         </div>
       </div>
@@ -55,10 +56,10 @@
     <!--Robe 4-->
     <div class="col-sm-6 col-md-2">
       <div class="thumbnail">
-        <img src="Contenu/images/robe_cocktail_verte.jpg" title="Robe de cocktail verte" />
+        <img class="robeCocktail" src="Contenu/images/robe_cocktail_verte.jpg" title="Robe de cocktail verte" />
         <div class="caption">
           <h3>Robe de cocktail verte</h3>
-          <p>Grâce à cette ravissante robe vous serez la plus belle pour allez danser et rendrez jalouse toutes les femmes.<br />EUR 79.99</p>
+          <!--<p>Grâce à cette ravissante robe vous serez la plus belle pour allez danser et rendrez jalouse toutes les femmes.--><br />EUR 79.99
           <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
         </div>
       </div>
@@ -69,10 +70,10 @@
     <!--Robe 5-->
     <div class="col-sm-6 col-md-2">
       <div class="thumbnail">
-        <img src="Contenu/images/robe_cocktail_bleue.jpg" title="Robe de cocktail bleue" />
+        <img class="robeCocktail" src="Contenu/images/robe_cocktail_bleue.jpg" title="Robe de cocktail bleue" />
         <div class="caption">
           <h3>Robe de cocktail bleue</h3>
-          <p>Cette robe fera de vous la plus convoitée femmes !<br /><br />EUR 79.99</p>
+          <!--<p>Cette robe fera de vous la plus convoitée femmes !--><br /><br /><br />EUR 79.99
           <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
         </div>
       </div>
@@ -80,10 +81,10 @@
     <!--Robe 6-->
     <div class="col-sm-6 col-md-2">
       <div class="thumbnail">
-        <img src="Contenu/images/robe_cocktail_violette.jpg" title="Robe de cocktail violette" />
+        <img class="robeCocktail" src="Contenu/images/robe_cocktail_violette.jpg" title="Robe de cocktail violette" />
         <div class="caption">
           <h3>Robe de cocktail violette</h3>
-          <p>Cette robe d'une extreme tendance est parfaite pour les femmes tentatrice.<br /><br />EUR 79.99</p>
+          <!--<p>Cette robe d'une extreme tendance est parfaite pour les femmes tentatrice.--><br />EUR 79.99
           <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
         </div>
       </div>
@@ -91,10 +92,10 @@
     <!--Robe 7-->
     <div class="col-sm-6 col-md-2">
       <div class="thumbnail">
-        <img src="Contenu/images/robe_cocktail_blanche.jpg" title="Robe de cocktail blanche" />
+        <img class="robeCocktail" src="Contenu/images/robe_cocktail_blanche.jpg" title="Robe de cocktail blanche" />
         <div class="caption">
           <h3>Robe de cocktail blanche</h3>
-          <p>Cette robe est tellement brillante qu'elle fera de vous la plus étincelante lorsque vous la portrez.<br />EUR 85.99</p>
+          <!--<p>Cette robe est tellement brillante qu'elle fera de vous la plus étincelante lorsque vous la portrez.--><br /><br />EUR 85.9
           <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
         </div>
       </div>
@@ -102,10 +103,10 @@
     <!--Robe 8-->
     <div class="col-sm-6 col-md-2">
       <div class="thumbnail">
-        <img src="Contenu/images/robe_cocktail_corail.jpg" title="Robe de cocktail corail" />
+        <img class="robeCocktail" src="Contenu/images/robe_cocktail_corail.jpg" title="Robe de cocktail corail" />
         <div class="caption">
           <h3>Robe de cocktail corail</h3>
-          <p>En portant ce tissu dorée vous deviendrez la plus lumineuse des femmes.<br /><br />EUR 89.99</p>
+          <!--<p>En portant ce tissu dorée vous deviendrez la plus lumineuse des femmes.--><br /><br />EUR 89.99
           <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
         </div>
       </div>
--- robeSoiree.php
+++ robeSoiree.php
@@ -14,6 +14,7 @@
     <div class="row">
         <div class="col-sm-6 col-md-2">
             <ul class="listMenuStyle">
+                <h1 class="enTete">STYLES</h1>
                 <a href="robeSoiree.php"><li>Robe de soirée</li></a>
                 <a href="robeCocktail.php"><li>Robe de cocktail</li></a>
                 <a href="robeMariee.php"><li>Robe de mariée</li></a>
@@ -25,7 +26,7 @@
           <a class="thumbnail" href="details.php"><img class="robeSoiree" src="Contenu/images/robe_soiree_rouge.jpg" title="Robe de soirée rouge tendance" /></a>
         <div class="caption">
           <h3>Robe de soirée rouge tendance</h3>
-          <p><br />EUR 129.99</p>
+          <br />EUR 129.99
           <p><a href="details.php" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
         </div>
       </div>
@@ -36,7 +37,7 @@
         <img class="robeSoiree" src="Contenu/images/robe_soiree_blanche.jpg" title="Robe de soirée blanche étincelante" />
         <div class="caption">
           <h3>Robe de soirée blanche étincelante</h3>
-          <p>Cette robe est tellement brillante qu'elle fera de vous la plus étincelante lorsque vous la portrez.<br />EUR 109.99</p>
+          <!--<p>Cette robe est tellement brillante qu'elle fera de vous la plus étincelante lorsque vous la portrez.-->EUR 109.99
           <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
         </div>
       </div>
@@ -47,7 +48,7 @@
         <img class="robeSoiree" src="Contenu/images/robe_soiree_verte.jpg" title="Robe de soirée verte audacieuse" />
         <div class="caption">
           <h3>Robe de soirée verte audacieuse</h3>
-          <p>Grâce à cette ravissante robe vous serez la plus belle pour allez danser et rendrez jalouse toutes les femmes.<br />EUR 100.99</p>
+          <!--<p>Grâce à cette ravissante robe vous serez la plus belle pour allez danser et rendrez jalouse toutes les femmes.--><br />EUR 100.99
           <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
         </div>
       </div>
@@ -58,12 +59,12 @@
         <img class="robeSoiree" src="Contenu/images/robe_soiree_bleueFonce.jpg" title="Robe de soirée bleue séductrice" />
         <div class="caption">
           <h3>Robe de soirée bleue séductrice</h3>
-          <p>En portant cette robe aucun homme ne pourra vous resister. N'hésitez plus elle est faite pour vous !<br />EUR 129.99</p>
+          <!--<p>En portant cette robe aucun homme ne pourra vous resister. N'hésitez plus elle est faite pour vous !--><br />EUR 129.99
           <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
         </div>
       </div>
     </div>
-  </div><br />
+  </div>
   <div class="row">  
       <div class="col-sm-6 col-md-2" id="cache"></div>
     <!--Robe 5-->
@@ -72,7 +73,7 @@
           <img class="robeSoiree" src="Contenu/images/robe_soiree_bleueClaire.jpg" title="Robe de soirée bleue attirante" />
           <div class="caption">
             <h3>Robe de soirée bleue attirante</h3>
-            <p>Cette robe fera de vous la plus convoitée femmes !<br />EUR 104.99</p>
+            <!--<p>Cette robe fera de vous la plus convoitée femmes !--><br />EUR 104.99
             <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
           </div>
         </div>
@@ -83,7 +84,7 @@
           <img class="robeSoiree" src="Contenu/images/robe_soiree_jaune.jpg" title="Robe de soirée dorée lumineuse" />
           <div class="caption">
             <h3>Robe de soirée dorée lumineuse</h3>
-            <p>En portant ce tissu dorée vous deviendrez la plus lumineuse des femmes.<br />EUR 139.99</p>
+            <!--<p>En portant ce tissu dorée vous deviendrez la plus lumineuse des femmes.--><br />EUR 139.99
             <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
           </div>
         </div>
@@ -94,7 +95,7 @@
           <img class="robeSoiree" src="Contenu/images/robe_soiree_violette.jpg" title="Robe de soirée violette tentatrice" />
           <div class="caption">
             <h3>Robe de soirée violette tentatrice</h3>
-            <p>Cette robe d'une extreme tendance est parfaite pour les femmes tentatrice.<br />EUR 119.99</p>
+            <!--<p>Cette robe d'une extreme tendance est parfaite pour les femmes tentatrice.--><br />EUR 119.99
             <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
           </div>
         </div>
@@ -105,7 +106,7 @@
           <img class="robeSoiree" src="Contenu/images/robe_soiree_beige.jpg" title="Robe de soirée beige élégante" />
           <div class="caption">
             <h3>Robe de soirée bleue attirante</h3>
-            <p>Parfaite pour une soirée chic, cette robe élégante vous ira à ravir !<br />EUR 104.99</p>
+            <!--<p>Parfaite pour une soirée chic, cette robe élégante vous ira à ravir !--><br />EUR 104.99
             <p><a href="#" class="btn btn-primary">Voir détails</a> <a href="#" class="btn btn-default">Panier</a></p>
           </div>
         </div>
