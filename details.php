7b8609ddbd160f5a3a02e0131b66494980a021c0
ggome <gomez.gwendo@gmail.com>
2013-10-04 13:16:52 +0200

    Modification page détails

--- details.php
+++ details.php
@@ -29,25 +29,29 @@
             </div>-->
             <div class="col-md-8">
                 <center>
-                    <h1>Robe de soirée rouge tendance</h1>
+                    <h1 class="titreDetail">Robe De Soirée Rouge Tendance</h1>
                     <table class="detailRobe">
                         <tr>
                             <td rowspan="2">
                                  <img class="imgRobeSoireeRouge" src="Contenu/images/robe_soiree_rouge.jpg" />v
                             </td>
                             <td class="RobeDetailTD">
-                                <p>
+                                <p class="detail">
                                     <u>Description</u> :<br /><br />
-                                    
+
                                     Matière : 96% polyester, 4% élasthanne<br />
                                     Entretien : Lavage à la main uniquement<br />
                                     Prix : 129.99€<br />
-                                    Temps de livraison : 6 jours;
+                                    Temps de livraison : 6 jours
                                 </p>
+                            </td>
+                            <td class="btnPanier">
+                                <a href="#" class="btn btn-default glyphicon glyphicon-shopping-cart"> Panier</a>
+                            </td>
                             </td>
                         </tr>
                         <tr>
-                            <td class="detailRobetr">
+                            <td class="detailRobetr" colspan="2">
                                 <p>Vous avez envie de vous faire remarquer ? Cette robe extremement tendance est parfaite pour vous !</p>
                             </td>
                         </tr>
