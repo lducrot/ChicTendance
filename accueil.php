4242381eb300a712ebfa583639ebb327cb606a7b
ggome <gomez.gwendo@gmail.com>
2013-10-08 09:50:29 +0200

    Ajout BD

--- accueil.php
+++ accueil.php
@@ -1,3 +1,9 @@
+<?php
+
+    // Connexion à la base de données desert
+    $bdd = new PDO("mysql:host=localhost;dbname=chicTendance;charset=utf8", "userCT", "ct",
+                    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+?>
 <!DOCTYPE html>
 <html>
     <head>
