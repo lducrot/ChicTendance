bc1b485feb16799446a3ab50b0fbecdc53d3666b
ggome <gomez.gwendo@gmail.com>
2013-10-15 08:35:08 +0200

    Connexion BD

--- Modele.php
+++ Modele.php
@@ -1,5 +1,7 @@
 <?php
-
-$bdd = new PDO("mysql:host=localhost;dbname=chicTendance;charset=utf8", "userCT", "ct", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-
+function getGenre() {
+    $bdd = new PDO("mysql:host=localhost;dbname=chicTendance;charset=utf8", "userCT", "ct", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $stmtGenre = $bdd->query("SELECT * FROM t_style ORDER BY styl_id");
+    $stmtStyle = $bdd->prepare("SELECT * FROM t_style WHERE styl_id=?");
+}
 ?>
