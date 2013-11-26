3e7562ed8aa7b34b556a09820421805d306d96a5
ggome <gomez.gwendo@gmail.com>
2013-11-26 09:14:36 +0100

    phase 6 en cours

--- Vue/Accueil/index.php
+++ Vue/Accueil/index.php
@@ -12,20 +12,20 @@ require 'Vue/_Commun/menuNavigation.php'; ?>
         <center>
             <table class="imgAccueil">
                 <tr>
-                    <td rowspan="2">
+                    <td rowspan="2" class="tdAccueilRobeSoiree">
                         <img class="imgAccueilRobeSoiree" src="Contenu/images/robe_soiree_bleueFonce.jpg" />
                     </td>
-                    <td>
+                    <td class="tdAccueilRobeMariee">
                         <img class="imgAccueilRobeMariee" src="Contenu/images/robe_mariee_4.jpg" />
                     </td>
                 </tr>
                 <tr>
-                    <td>
+                    <td class="tdAccueilRobeMariee">
                         <img class="imgAccueilRobeCocktail" src="Contenu/images/robe_cocktail_rouge.jpg" />
                     </td>
                 </tr>
             </table>
-            <p>Chic Tendance vous présente ces plus belles créations en robes de soirées, robes de mariée et robes de cocktails ! Grâce à elles vous êtes sûr d'être à la pointe de la mode. Il y en a assez pour satisfaire tout les goûts, alors n'hésitez plus !</p>
+            <p>Chic Tendance vous présente ses plus belles créations en robes de soirées, robes de mariée et robes de cocktails ! Grâce à elles vous êtes sûr d'être à la pointe de la mode. Il y en a assez pour satisfaire tout les goûts, alors n'hésitez plus !</p>
         </center> 
     </div>
 </div>
