490ff3b76c663d0fcbab95be53c6fd1f05d40c19
ggome <gomez.gwendo@gmail.com>
2013-12-02 12:21:35 +0100

    phase 6 en cours

---
+++ Modele/Client.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'Framework/Modele.php';
+
+class Client extends Modele {
+    
+    private $sqlClient = "SELECT * FROM t_client ";
+    
+    public function getClient($clie_courriel, $clie_mdp) {
+        $req = $this->sqlRobe . "where clie_id='?' and clie_mdp='?'";
+        $client = $this->executerRequete($req, array($clie_courriel, $clie_mdp));
+        if ($client->rowCount() == 1)
+            return $client;
+        else
+            throw new Exception ("L'utilisateur n'a pas été reconnu");
+    }
+}
