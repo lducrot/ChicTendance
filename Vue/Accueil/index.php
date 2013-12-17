64d68ff79a770a89404a9d88582e42ebf8b5d428
lducr <lauriane.ducrot@gmail.com>
2013-12-17 08:09:01 +0100

    modif

--- Vue/Accueil/index.php
+++ Vue/Accueil/index.php
@@ -1,13 +1,18 @@
-<?php $this->titre = "Chic Tendance"; 
-require 'Vue/_Commun/menuNavigation.php'; 
-require 'Vue/_Commun/barreNavigation.php';?>
+<?php
+$this->titre = "Chic Tendance";
+require 'Vue/_Commun/menuNavigation.php';
+require 'Vue/_Commun/barreNavigation.php';
+?>
 
 <ul class="breadcrumb">
     <li><a class="filAriane" href="index.php">Accueil</a></li>
 </ul>
+
 <div class="row">
+    
     <div class="col-md-1">
     </div>
+    
     <div class="col-md-11">
         <center>
             <table class="imgAccueil">
@@ -28,5 +33,6 @@ require 'Vue/_Commun/barreNavigation.php';?>
             <p>Chic Tendance vous présente ses plus belles créations en robes de soirées, robes de mariée et robes de cocktails ! Grâce à elles vous êtes sûr d'être à la pointe de la mode. Il y en a assez pour satisfaire tout les goûts, alors n'hésitez plus !</p>
         </center> 
     </div>
+    
 </div>
 
